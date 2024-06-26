26cf57a4cca9e41af9d13cb41d24bd04d9ed8e4f
Abdelhay zaadaddi <abdezaadaddi@gmail.com>
2024-06-26 10:51:45 +0000

    4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-""" File LRU cache """
+""" File mru cache """
 
 from base_caching import BaseCaching
 
 
-class LRUCache(BaseCaching):
-    """ LRU cache class """
+class MRUCache(BaseCaching):
+    """ mru cache class """
+
     def __init__(self):
         super().__init__()
         self.use_order = []
@@ -14,9 +15,9 @@ class LRUCache(BaseCaching):
         """ put method """
         if key is not None and item is not None:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                lru_key = self.use_order.pop(0)
-                print("DISCARD: {}".format(lru_key))
-                del self.cache_data[lru_key]
+                mru_key = self.use_order.pop()
+                print("DISCARD: {}".format(mru_key))
+                del self.cache_data[mru_key]
             self.cache_data[key] = item
             if key in self.use_order:
                 self.use_order.remove(key)
